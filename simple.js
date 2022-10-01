c0aa032b4c190e11d1a96b7b98e2bf255799cef8
Inga Kristine <inga_kristine@hotmail.com>
2022-10-01 11:52:00 +0200

    Solved a merge conflict

--- simple.js
+++ simple.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-console.log("Hello world! I just changed this file");
-=======
 console.log("Hello world! This sentence is written from the GitHub platform");
->>>>>>> 7ffd0ab5e5044c86399c0079433bdf5164bd17f3
